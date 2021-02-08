61c7d7303bbd33641a66e462567f6b14d5d4e30a
Talita Groppo <talitagroppo@icloud.com>
2021-02-08 15:57:35 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Talita Groppo on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
